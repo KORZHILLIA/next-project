52848906ae670fcf98eb3ce2854f98db785e853e
KORZH ILLIA <kiev_drum2006@ukr.net>
2023-05-22 13:56:14 +0300

    Provide 404 Page

---
+++ pages/404.tsx
@@ -0,0 +1,30 @@
+import { useContext } from "react";
+import { useRouter } from "next/router";
+
+import Head from "next/head";
+
+import Header from "@/shared/components/Header/header";
+import Button from "@/shared/components/Button/Button";
+
+import { ThemeContext, ThemeContextType } from "@/context/themeContext";
+
+
+export default function Page404() {
+    const { isDark } = useContext(ThemeContext) as ThemeContextType;
+    const router = useRouter();
+    
+    return (
+    <>
+        <Head>
+            <title>СпортТовари</title>
+            <meta property='og:title' content='СпортТовари' key='title' />
+            <meta name="description" content="Сайт, присвячений спортивним товарам" />
+            </Head>
+            <Header />
+            <main className={`w-full h-screen ${isDark ? 'bg-slate-600' : 'bg-slate-200'}`}>
+                <h1 className={`text-center text-4xl md:text-6xl mb-4 pt-[calc(50vh-54px)] ${isDark ? 'text-white' : 'text-black'}`}>Page Not Found</h1>
+                <Button onBtnClick={() => router.back()} type="button" text="Повернутися" styles={`block max-w-[200px] mx-auto p-2 bg-sky-600 text-center text-2xl`} />
+        </main>
+        </>
+    );
+}
--- shared/components/Button/Button.tsx
+++ shared/components/Button/Button.tsx
@@ -2,10 +2,11 @@ type ButtonProps = {
     type: 'button' | 'submit' | 'reset',
     text: string,
     styles: string,
+    onBtnClick?: (event: React.MouseEvent) => void,
 }
 
 export default function Button(props: ButtonProps) {
-    return (<button type={props.type}
+    return (<button onClick={props.onBtnClick} type={props.type}
         className={`p-2 flex justify-center items-center border border-zinc-400 rounded-md ${props.styles}`}>
         {props.text}
     </button>);
