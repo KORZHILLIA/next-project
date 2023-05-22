56e9d5653023e3db8badc7b23a7540bdce360d79
KORZH ILLIA <kiev_drum2006@ukr.net>
2023-05-22 08:52:01 +0300

    Change lang attribute, change README.md

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document({children}: {children: React.ReactNode}) {  
   return (
-    <Html lang="en">
+    <Html lang="uk">
       <Head/>
       <body>
         <Main />
